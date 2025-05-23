58276eed4c5f13b104699aa95cb1371da99ae882
efrenurena <efren@limosys.com>
2025-05-23 11:30:54 -0400

    dissable background scrolling

--- main.js
+++ main.js
@@ -112,12 +112,28 @@ const menuBackdrop = document.getElementById('menu-backdrop');
 function toggleMenu(open = null) {
     const isVisible = menuPanel.classList.contains('visible');
     const shouldOpen = open === null ? !isVisible : open;
-
+    toggleScroll(!shouldOpen);
     menuPanel.classList.toggle('visible', shouldOpen);
     menuBackdrop.classList.toggle('visible', shouldOpen);
 }
 hamburger.addEventListener('click', () => toggleMenu());
 menuBackdrop.addEventListener('click', () => toggleMenu(false));
+function toggleScroll(enable) {
+    if (enable) {
+        // unlock:
+        const scrollYValue = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.width = '';
+        window.scrollTo(0, parseInt(scrollYValue || '0') * -1);
+    } else {
+        // lock
+        const scrollY = window.scrollY;
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${scrollY}px`;
+        document.body.style.width = '100%';
+    }
+}
 
 // DICE THRONE DELETE
 const diceThroneDelete = document.getElementById('dice-throne-delete');
@@ -177,8 +193,6 @@ diceThroneDelete.addEventListener('click', () => {
         }
     });
 });
-
-
 
 // MARK: TOP BAR
 // UNDO
